59228e58b6cf40c3327bc7b22cf2315025387b2f
makyzi <sedlacek.martin9@gmail.com>
2020-08-21 18:18:55 +0200

    added postgresql, article Model

---
+++ blog_platform/article/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    article_id = models.AutoField(primary_key=True)
+    article_heading = models.CharField(max_length = 250)
+    article_body = models.TextField()
+    # article_date
+    # etc
+    # toto si potom poupravuj, len skusam django rest api
